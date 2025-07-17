27c83ff23186e1f464bcfdece1e53679de6a1549
Rahul <rahulrajkumar.personal@gmail.com>
2025-07-17 14:31:43 +0000

    updated links

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function HomePage() {
             <Card className="bg-transparent border-none hover:bg-gray-100 dark:hover:bg-gray-900/50 transition-all duration-300">
               <CardContent className="p-0">
                 <Link
-                  href="https://github.com/alt-rahul/5g-edge-app"
+                  href="https://gitlab.orbit-lab.org/rahulrajkumar/5g-edge-apps"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex items-center justify-between p-6 group"
